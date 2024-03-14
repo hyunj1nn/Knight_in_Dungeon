f3ea93eb488e8047cd9f6ada048c18c1e160d529
HyunJin <137889239+hyunj1nn@users.noreply.github.com>
2024-03-14 20:53:28 +0900

    주석 수정

--- Assets/KnightSurvivor/Codes/AnimationAutoDestroy.cs
+++ Assets/KnightSurvivor/Codes/AnimationAutoDestroy.cs
@@ -6,19 +6,18 @@ public class AnimationAutoDestroy : MonoBehaviour
 {
     void Start()
     {
-        // 애니메이터 컴포넌트를 가져옵니다.
         Animator animator = GetComponent<Animator>();
 
-        // 애니메이터가 있는 경우, 애니메이션 클립의 길이를 가져옵니다.
+        // 애니메이터가 있는 경우, 애니메이션 클립의 길이를 가져옴
         float duration = 0;
         if (animator)
         {
-            // 현재 재생중인 애니메이션 클립을 가져옵니다.
+            // 현재 재생중인 애니메이션 클립을 가져옴
             AnimationClip clip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
             duration = clip.length;
         }
 
-        // 지정된 시간이 지난 후에 자신을 삭제합니다.
+        // 지정된 시간이 지난 후에 자신을 삭제
         Destroy(gameObject, duration);
     }
 }
